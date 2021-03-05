97f3a197cfb75ed1bf3c52931a1ca2a045eaa74b
Remy.Jeker <remy.jeker@quebecormedia.com>
2021-03-04 19:24:02 -0500

    responsive layout / base gameplay (algorithm)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,96 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Puissance 4';
+  title: string = 'Puissance 4';
 
-  numberColumns = 7;
-  numberRows = 6;
+  playerYellow: number = 1;
+  playerRed: number = 2;
+
+  numberColumns: number = 7;
+  numberRows: number = 6;
 
   columns = new Array(this.numberColumns);
   rows = new Array(this.numberRows);
 
-  setToken(colNum, rowNum) {
-    console.log('setToken', colNum, rowNum)
+  board: Array<Array<number>>;
+  height: Array<number>;
+  moves: Array<number>;
+  won: boolean;
+
+  ngOnInit() {
+    this.initValues();
+  }
+
+  initValues() {
+    this.board = new Array();
+    for (let index = 0; index < this.numberColumns; index++) {
+      this.board.push(new Array(this.numberRows).fill(0));
+    }
+    this.height = new Array(this.numberColumns).fill(0);
+    this.moves = new Array();
+    this.won = false;
+  }
+
+  canPlay(col: number) {
+    return (!this.won && (this.height[col] < this.numberRows));
+  }
+
+  play(col: number) {
+    this.board[col][this.height[col]] = 1 + (this.moves.length % 2);
+    this.height[col]++;
+    this.moves.push(col);
+    if (this.moves.length < 7) return;
+    this.checkWin();
+  }
+
+  checkWin() {
+    var x = this.moves[this.moves.length - 1];
+    var y = this.height[x] - 1;
+  
+    if (y >= 3 && this.board[x][y - 3] == this.board[x][y] && this.board[x][y - 2] == this.board[x][y] && this.board[x][y - 1] == this.board[x][y]) {
+      this.showWin(x, y, 0, -1);
+      this.won = true;
+      return;
+    }
+
+    for (var dy = -1; dy <= 1; dy++) {
+      var nb = 0;
+      for (var dx = 1; x + dx < 7 && y + dx * dy < 6 && y + dx * dy >= 0; dx++)
+        if (this.board[x + dx][y + dx * dy] == this.board[x][y]) nb++;
+        else break;
+      for (var dx = -1; x + dx >= 0 && y + dx * dy < 6 && y + dx * dy >= 0; dx--)
+        if (this.board[x + dx][y + dx * dy] == this.board[x][y]) nb++;
+        else break;
+      if (nb >= 3) {
+        this.showWin(x + dx + 1, y + (dx + 1) * dy, 1, dy);
+        this.won = true;
+        return;
+      }
+    }
+  }
+
+  showWin(x: number, y: number, dx: number, dy: number) {
+    alert(`WON at position : ${x}, ${y}, ${dx}, ${dy}`);
+  }
+
+  // template functions
+
+  setToken(col: number) {
+    if (this.canPlay(col)) {
+      this.play(col);
+    } else {
+      if (this.won) alert('Play again ?');
+      else alert('Play in another column ;)');
+    }
+  }
+
+  currentPlayer() {
+    return (this.moves.length % 2)
+      ? 'Joueur Rouge'
+      : 'Joueur Jaune';
+  }
+
+  restartGame() {
+    this.initValues();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
